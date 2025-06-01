a17faf384ac470239e32185d34c59cc653bdbca4
Benjamin <benhasanemail@gmail.com>
2025-06-01 16:43:21 -0700

    add visual hotbar, interaction next (#5)

--- src/screens/gameplay.rs
+++ src/screens/gameplay.rs
@@ -6,6 +6,7 @@ use crate::{Pause, demo::level::spawn_level, menus::Menu, screens::Screen};
 
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(OnEnter(Screen::Gameplay), spawn_level);
+    app.add_plugins(crate::gameplay::plugin);
 
     // Toggle pause on key press.
     app.add_systems(
