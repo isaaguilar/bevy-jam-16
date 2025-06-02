a88cd9c4362d066b2131147f1eb22147810db135
Benjamin <benhasanemail@gmail.com>
2025-06-01 23:11:02 -0700

    tooltip & despawn

--- src/gameplay/hotbar.rs
+++ src/gameplay/hotbar.rs
@@ -1,28 +1,28 @@
 use crate::data::*;
 use crate::prelude::*;
+use crate::theme::prelude::*;
 use bevy::ecs::spawn::*;
 use bevy::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(OnEnter(Screen::Gameplay), spawn_turret_bar);
+    app.add_systems(OnEnter(Screen::Gameplay), on_enter_game);
     app.add_systems(
         Update,
         (highlight_hovered_tile, on_press_hotbar)
             .in_set(PausableSystems)
             .run_if(in_state(Screen::Gameplay)),
     );
+
+    app.add_systems(Update, watch_pointer_state);
 }
 
 #[derive(Component, Debug, Reflect)]
-struct HotbarItem();
+struct HotbarItem;
 
-fn spawn_turret_bar(mut commands: Commands, assets: Res<UiAssets>) {
-    // let hotbar_items = vec![
-    //     (Tower::Tesla, assets.hotbar_tesla_image.clone()),
-    //     (Tower::Water, assets.hotbar_water_image.clone()),
-    //     (Tower::TrapDoor, assets.hotbar_trapdoor_image.clone()),
-    // ];
+#[derive(Component)]
+struct CancelInput;
 
+fn on_enter_game(mut commands: Commands, assets: Res<UiAssets>) {
     let hotbar_items: Vec<_> = Tower::all()
         .iter()
         .map(|t| {
@@ -42,6 +42,54 @@ fn spawn_turret_bar(mut commands: Commands, assets: Res<UiAssets>) {
                 .map(|(tower, icon)| spawn_hotbar_item(tower, icon)),
         )),
     ));
+
+    commands
+        .spawn((
+            StateScoped(Screen::Gameplay),
+            CancelInput,
+            Visibility::Hidden,
+            BackgroundColor(Color::Srgba(Srgba::RED.with_alpha(0.5))),
+            Node {
+                position_type: PositionType::Absolute,
+                left: Val::Px(8.0),
+                bottom: Val::Px(8.0),
+                width: Val::Px(64.0),
+                height: Val::Px(64.0),
+                ..default()
+            },
+            children![(Text::new("X"), TextFont::from_font_size(64.0))],
+        ))
+        .observe(on_click_cancel);
+}
+
+fn watch_pointer_state(
+    mut changes: EventReader<StateTransitionEvent<PointerInteractionState>>,
+    mut query: Query<&mut Visibility, With<CancelInput>>,
+) {
+    for change in changes.read() {
+        if let Some(state) = change.entered {
+            match state {
+                PointerInteractionState::Placing(_) => {
+                    for mut visibility in query.iter_mut() {
+                        *visibility = Visibility::Inherited;
+                    }
+                }
+                _ => {
+                    for mut visibility in query.iter_mut() {
+                        *visibility = Visibility::Hidden;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn on_click_cancel(
+    _: Trigger<Pointer<Released>>,
+    mut pointer_input_state: ResMut<NextState<PointerInteractionState>>,
+) {
+    println!("Clicked on click");
+    pointer_input_state.set(PointerInteractionState::Selecting)
 }
 
 fn spawn_hotbar() -> impl Bundle {
@@ -52,10 +100,10 @@ fn spawn_hotbar() -> impl Bundle {
             position_type: PositionType::Absolute,
             bottom: Val::Px(8.0),
             margin: UiRect::horizontal(Val::Auto),
-            height: Val::Px(88.0),
+            // height: Val::Px(88.0),
             padding: UiRect::axes(Val::Px(8.0), Val::Px(8.0)),
             display: Display::Flex,
-            column_gap: Val::Px(8.0),
+            column_gap: Val::Px(12.0),
             align_items: AlignItems::Center,
             justify_content: JustifyContent::Center,
             flex_direction: FlexDirection::Row,
@@ -70,22 +118,42 @@ fn spawn_hotbar_item(tower: Tower, icon: Handle<Image>) -> impl Bundle {
         Name::new(tower.name()),
         Button,
         Node {
-            width: Val::Px(72.),
-            height: Val::Px(72.),
+            position_type: PositionType::Relative,
+            border: UiRect::all(Val::Px(2.0)),
             ..default()
         },
         BackgroundColor(Color::WHITE.with_alpha(0.25)),
         BorderColor(Color::WHITE),
         BorderRadius::all(Val::Px(8.0)),
-        HotbarItem(),
+        HotbarItem,
         tower,
-        children![(
-            Node {
-                padding: UiRect::all(Val::Px(4.0)),
-                ..default()
-            },
-            ImageNode::new(icon)
-        )],
+        TooltipParent,
+        children![
+            (
+                Tooltip,
+                BackgroundColor(Color::BLACK.with_alpha(0.75)),
+                BorderRadius::all(Val::Px(8.0)),
+                BorderColor(Color::WHITE),
+                Node {
+                    border: UiRect::all(Val::Px(2.0)),
+                    padding: UiRect::all(Val::Px(8.0)),
+                    position_type: PositionType::Absolute,
+                    top: Val::Px(-56.0),
+                    max_width: Val::Px(256.0),
+                    ..default()
+                },
+                children![Text(tower.name().into()),]
+            ),
+            (
+                Node {
+                    padding: UiRect::all(Val::Px(4.0)),
+                    width: Val::Px(64.0),
+                    height: Val::Px(64.0),
+                    ..default()
+                },
+                ImageNode::new(icon)
+            )
+        ],
     )
 }
 
