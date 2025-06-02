53e44354f6fa61578f75989b4f1a02b71bdf8234
Zellenon <zellenon@gmail.com>
2025-06-01 22:16:51 -0500

    added gravity to enemies (not working?)

--- src/demo/enemy.rs
+++ src/demo/enemy.rs
@@ -34,8 +34,11 @@ pub fn enemy_spawn_bundle(
             }),
             ..default()
         },
-        EnemyControllerBundle::new(Collider::round_rectangle(15., 20., 0.5), Vector::splat(0.))
-            .with_movement(max_speed, 0.96),
+        EnemyControllerBundle::new(
+            Collider::round_rectangle(15., 20., 0.5),
+            Vector::new(0., -200.0),
+        )
+        .with_movement(max_speed, 0.96),
         Transform::from_scale(Vec2::splat(0.2).extend(1.0))
             .with_translation(Vec3::new(0.0, 0.0, 1.)),
     )
