1feae02d8d5b32bb98820ae1d6f91bb68140f13d
Zellenon <zellenon@gmail.com>
2025-06-02 22:37:51 -0500

    removed legacy code

--- src/demo/level.rs
+++ src/demo/level.rs
@@ -29,17 +29,6 @@ pub fn spawn_level(
     mut level: ResMut<Level>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
-    commands.spawn((
-        Name::new("Level"),
-        Transform::default(),
-        Visibility::default(),
-        StateScoped(Screen::Gameplay),
-        children![
-            //enemy_spawn_bundle(30.0, &assets, &mut texture_atlas_layouts),
-            //player(40.0, &assets, &mut texture_atlas_layouts),
-            (Name::new("Gameplay Music"), music(assets.music.clone())),
-        ],
-    ));
     *level = Level::from_str(MAP_TEXT);
     commands.compose(
         LevelParent::from_data(&level)
