065753e5a3fbbb2d48e737df5c04c56cdc58ae6c
Zellenon <zellenon@gmail.com>
2025-06-03 14:09:39 -0500

    added tower module

--- src/gameplay/mod.rs
+++ src/gameplay/mod.rs
@@ -1,7 +1,8 @@
 use bevy::prelude::*;
 
-mod hotbar;
-mod tower_placement;
+pub mod hotbar;
+pub mod tower_placement;
+pub mod towers;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((hotbar::plugin, tower_placement::plugin));
