a4de139d2da09dd8c08e130f1ab02958094e8520
Zellenon <zellenon@gmail.com>
2025-06-03 02:51:22 -0500

    fixed merge error

--- src/gameplay/mod.rs
+++ src/gameplay/mod.rs
@@ -2,14 +2,14 @@ use bevy::prelude::*;
 
 pub mod hotbar;
 pub mod level;
-pub mod turret_placement;
+pub mod tower_placement;
 pub mod wave_manager;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((
         hotbar::plugin,
         level::plugin,
-        turret_placement::plugin,
+        tower_placement::plugin,
         wave_manager::plugin,
     ));
 }
