b73f3bde7e30f5fe87b60c5f540d59e0ec942eae
Zellenon <zellenon@gmail.com>
2025-06-03 20:12:22 -0500

    import cleanup, added second tower at an angle for testing

--- src/gameplay/towers/mod.rs
+++ src/gameplay/towers/mod.rs
@@ -4,18 +4,18 @@ use bevy::{
         schedule::IntoScheduleConfigs,
         system::{Commands, Res},
     },
+    math::{Quat, Vec3},
     state::{condition::in_state, state::OnEnter},
     transform::components::Transform,
 };
-use bevy_composable::app_impl::ComplexSpawnable;
+use bevy_composable::app_impl::{ComplexSpawnable, ComponentTreeable};
+use std::f32;
 
 use crate::{
     PausableSystems, assets::TowerSprites, data::Tower, level::components::pos,
     prefabs::towers::tower, screens::Screen,
 };
-use common::{
-    add_tower_targets_from_zone, remove_cooldown, remove_tower_targets, tick_cooldown, towers_fire,
-};
+use common::*;
 
 pub mod common;
 pub mod tesla;
@@ -40,4 +40,10 @@ pub(super) fn plugin(app: &mut App) {
 
 pub fn test_system(mut commands: Commands, sprites: Res<TowerSprites>) {
     commands.compose(tower(Tower::Tesla) + pos(30., 30.));
+    commands.compose(
+        tower(Tower::Tesla)
+            + Transform::from_xyz(0., 20., 0.)
+                .with_rotation(Quat::from_axis_angle(Vec3::Z, -f32::consts::FRAC_PI_2))
+                .store(),
+    );
 }
